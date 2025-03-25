3993c93be665aa5106a0ce8e7097fe00cfe7b782
Haeran Kim <116875965+kimmand0o0@users.noreply.github.com>
2025-03-25 22:05:45 +0900

    [Bronze V] Title: 꼬마 정민, Time: 92 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.js
@@ -0,0 +1,8 @@
+var fs = require('fs');
+var input = fs.readFileSync('/dev/stdin').toString().split(' ')
+
+var A = parseInt(input[0]);
+var B = parseInt(input[1]);
+var C = parseInt(input[2]);
+
+console.log(A+B+C);

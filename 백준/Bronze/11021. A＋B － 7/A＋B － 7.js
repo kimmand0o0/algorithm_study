0d18cfd22662ed8bdfb82b9feb2e3d9c7235ff2b
Haeran Kim <116875965+kimmand0o0@users.noreply.github.com>
2025-04-11 12:11:48 +0900

    [Bronze V] Title: A+B - 7, Time: 96 ms, Memory: 9528 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,10 @@
+var fs = require('fs');
+var input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+var caseLength = parseInt(input[0])
+
+for(let i = 0; i < caseLength; i++){
+	var [A, B] = input[i + 1].split(' ').map((item) => +item);
+	
+	console.log(`Case #${i+1}: ${A+B}`)
+}
